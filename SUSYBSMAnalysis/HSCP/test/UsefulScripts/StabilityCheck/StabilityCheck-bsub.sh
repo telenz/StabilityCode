9cd55544a3dae35bfc8626669c0336eae84fb171
Teresa Lenz <teresa.lenz@desy.de>
2014-08-27 18:33:37 +0200

    change path

--- SUSYBSMAnalysis/HSCP/test/UsefulScripts/StabilityCheck/StabilityCheck-bsub.sh
+++ SUSYBSMAnalysis/HSCP/test/UsefulScripts/StabilityCheck/StabilityCheck-bsub.sh
@@ -1,5 +1,8 @@
 #!/bin/bash/
-ppp=/afs/cern.ch/user/t/telenz/work/CMSSW_5_3_14_patch2/src/SUSYBSMAnalysis/HSCP/test/UsefulScripts/StabilityCheck/${folder2}/${name}
+SCRIPTPATH=`pwd -P`
+echo ${SCRIPTPATH}
+ppp=${SCRIPTPATH}/${folder2}/${name}
+#ppp=/afs/cern.ch/user/t/telenz/work/CMSSW_5_3_14_patch2/src/SUSYBSMAnalysis/HSCP/test/UsefulScripts/StabilityCheck/${folder2}/${name}
 cd $ppp
 echo $ppp  
 source /afs/cern.ch/cms/cmsset_default.sh
